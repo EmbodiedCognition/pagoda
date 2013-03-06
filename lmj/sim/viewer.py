de9302b55e86b181372872d5661fa634374f436b
Leif Johnson <leif@leifjohnson.net>
2013-03-05 18:23:55 -0600

    Add shadows (thank you interwebs !!).

--- lmj/sim/viewer.py
+++ lmj/sim/viewer.py
@@ -42,16 +42,22 @@ class Null(object):
 
 
 class GL(glumpy.Figure):
-    def __init__(self, world, trace=False, paused=False, distance=30):
+    def __init__(self, world, trace=None, paused=False, distance=30):
         super(GL, self).__init__()
         self.world = world
         self.trace = trace
+        self.twirl = False
         self.paused = paused
         self.elapsed = 0
         self.lens = self.add_frame()
         self.trackball = glumpy.Trackball(65, 30, 1, distance)
         self._x = 0
         self._y = 0
+        glut.glutInitDisplayMode(
+            glut.GLUT_DOUBLE |
+            glut.GLUT_RGBA |
+            glut.GLUT_DEPTH |
+            glut.GLUT_STENCIL)
 
     def noop(self, *args, **kwargs):
         pass
@@ -81,14 +87,15 @@ class GL(glumpy.Figure):
     def on_init(self):
         self.on_resize(800, 600)
 
-        gl.glEnable(gl.GL_BLEND)
-        gl.glEnable(gl.GL_LINE_SMOOTH)
-        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
-
+        gl.glColorMaterial(gl.GL_FRONT, gl.GL_AMBIENT_AND_DIFFUSE)
         gl.glEnable(gl.GL_COLOR_MATERIAL)
-        gl.glColorMaterial(gl.GL_FRONT_AND_BACK, gl.GL_AMBIENT_AND_DIFFUSE)
 
+        gl.glDepthFunc(gl.GL_LEQUAL)
         gl.glEnable(gl.GL_DEPTH_TEST)
+
+        gl.glShadeModel(gl.GL_SMOOTH)
+        gl.glHint(gl.GL_PERSPECTIVE_CORRECTION_HINT, gl.GL_NICEST)
+
         gl.glEnable(gl.GL_NORMALIZE)
 
         gl.glEnable(gl.GL_LIGHTING)
@@ -101,7 +108,11 @@ class GL(glumpy.Figure):
         gl.glLight(gl.GL_LIGHT0, gl.GL_DIFFUSE, [1, 1, 1, 1])
         gl.glLight(gl.GL_LIGHT0, gl.GL_SPECULAR, [1, 1, 1, 1])
 
-        gl.glShadeModel(gl.GL_FLAT)
+        # fog ? from http://www.swiftless.com/tutorials/opengl/fog.html
+        #gl.glFogi(gl.GL_FOG_MODE, gl.GL_EXP2)
+        #gl.glFogfv(gl.GL_FOG_COLOR, (0.5, 0,5, 0,5, 1.0))
+        #gl.glFogf(gl.GL_FOG_DENSITY, 0.2)
+        #gl.glHint(gl.GL_FOG_HINT, gl.GL_NICEST)
 
     def on_resize(self, width, height):
         w, h = float(width), float(height)
@@ -119,14 +130,25 @@ class GL(glumpy.Figure):
     def on_key_press(self, key, modifiers):
         if key == glumpy.window.key.ESCAPE:
             sys.exit()
-        if key == glumpy.window.key.SPACE:
+        elif key == glumpy.window.key.SPACE:
             self.paused = False if self.paused else True
+        elif key == glumpy.window.key.R:
+            self.twirl = False if self.twirl else True
         else:
             self.world.reset()
         self.redraw()
 
     def on_draw(self):
-        self.clear(0, 0, 0)
+        def ground():
+            '''Draw a 10-meter square on the ground plane.'''
+            gl.glBegin(gl.GL_QUADS)
+            gl.glColor(0.3, 0.3, 0.3, 0.7)
+            gl.glNormal(0, 0, 1)
+            gl.glVertex(-10,  10, 0)
+            gl.glVertex( 10,  10, 0)
+            gl.glVertex( 10, -10, 0)
+            gl.glVertex(-10, -10, 0)
+            gl.glEnd()
 
         # modified slightly from glumpy.trackball.Trackball.push
         _, _, w, h = gl.glGetIntegerv(gl.GL_VIEWPORT)
@@ -140,18 +162,48 @@ class GL(glumpy.Figure):
         gl.glMatrixMode(gl.GL_MODELVIEW)
         gl.glPushMatrix()
         gl.glLoadIdentity()
+
         gl.glTranslate(self._x, self._y, -self.trackball.distance)
         gl.glMultMatrixf(self.trackball.matrix)
 
-        # draw a square on the ground plane.
-        gl.glBegin(gl.GL_QUADS)
-        gl.glColor(0.2, 0.3, 0.4)
-        gl.glNormal(0, 0, 1)
-        gl.glVertex(-10,  10, 0)
-        gl.glVertex( 10,  10, 0)
-        gl.glVertex( 10, -10, 0)
-        gl.glVertex(-10, -10, 0)
-        gl.glEnd()
+        # shadows from http://www.swiftless.com/tutorials/opengl/basic_shadows.html
+        gl.glClearStencil(0)
+        gl.glClearDepth(1)
+        gl.glClearColor(1, 1, 1, 1)
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT |
+                   gl.GL_DEPTH_BUFFER_BIT |
+                   gl.GL_STENCIL_BUFFER_BIT)
+
+        gl.glColorMask(gl.GL_FALSE, gl.GL_FALSE, gl.GL_FALSE, gl.GL_FALSE)
+        gl.glDepthMask(gl.GL_FALSE)
+        gl.glEnable(gl.GL_STENCIL_TEST)
+        gl.glStencilFunc(gl.GL_ALWAYS, 1, 0xFFFFFFFF)
+        gl.glStencilOp(gl.GL_REPLACE, gl.GL_REPLACE, gl.GL_REPLACE)
+
+        ground()
+
+        gl.glColorMask(gl.GL_TRUE, gl.GL_TRUE, gl.GL_TRUE, gl.GL_TRUE)
+        gl.glDepthMask(gl.GL_TRUE)
+        gl.glStencilFunc(gl.GL_EQUAL, 1, 0xFFFFFFFF)
+        gl.glStencilOp(gl.GL_KEEP, gl.GL_KEEP, gl.GL_KEEP)
+
+        gl.glDisable(gl.GL_TEXTURE_2D)
+        gl.glDisable(gl.GL_DEPTH_TEST)
+        gl.glPushMatrix()
+        gl.glScale(1, 1, -1)
+
+        self.world.draw(color=(0, 0, 0, 0.3))
+
+        gl.glPopMatrix()
+        gl.glEnable(gl.GL_DEPTH_TEST)
+        gl.glDisable(gl.GL_STENCIL_TEST)
+
+        gl.glEnable(gl.GL_BLEND)
+        gl.glBlendFunc(gl.GL_SRC_ALPHA, gl.GL_ONE_MINUS_SRC_ALPHA)
+
+        ground()
+
+        gl.glDisable(gl.GL_BLEND)
 
         self.world.draw()
 
@@ -166,7 +218,10 @@ class GL(glumpy.Figure):
             if not self.world.step():
                 sys.exit()
             if self.trace:
-                self.world.trace()
+                self.world.trace(self.trace)
+            if self.twirl:
+                p = self.trackball.phi
+                self.trackball.phi = (p + 0.07) % 360
             if self.world.needs_reset():
                 self.world.reset()
             self.redraw()
