6db2bcbe800b6f6e4597c4268dfc24146670a420
Leif Johnson <leif@leifjohnson.net>
2014-08-16 11:34:57 -0500

    Replace email address.

--- pagoda/skeleton.py
+++ pagoda/skeleton.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Leif Johnson <leif@leifjohnson.net>
+# Copyright (c) 2013 Leif Johnson <leif@cs.utexas.edu>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
