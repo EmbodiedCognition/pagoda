def5ea5dcdd4ec16decf2d0b86fca91575df25d0
Leif Johnson <leif@leifjohnson.net>
2014-09-07 09:29:40 -0700

    Start to add ASF parser into parser class hierarchy.

--- pagoda/parser.py
+++ pagoda/parser.py
@@ -12,122 +12,7 @@ TAU = 2 * np.pi
 
 
 class Parser:
-    '''This parser class reads a text configuration of a pagoda simulation.
-
-    The format for the configuration file follows basic Unix conventions. A
-    preprocessor discards any text on a line following the pound (#) character.
-    Blank lines are skipped. The remainder of the file is chunked into sections
-    based on keywords; each keyword in the file indicates the start of a new
-    section. The recognized keywords are:
-
-    - body -- indicates the definition of a new rigid body;
-    - join -- indicates that two bodies will be joined together.
-
-    Bodies
-    ------
-
-    A "body" section must contain the following information:
-
-    - A shape specification. This must be one of box (rectangular prism), sph or
-      sphere (sphere), cyl or cylinder (cylinder), or cap (capped cylinder).
-
-      The shape indicates the canonical geometry that will be used to (a)
-      specify the size of the body, and (b) detect collisions.
-
-    - An identifier for the body. This cannot contain spaces.
-
-    - One or more dimension parameters. Each of these consists of a shape
-      keyword followed by one or more size in meters. Different dimension
-      keywords are required based on the shape of the body:
-
-      - box: length X Y Z
-      - sph: radius R
-      - cyl: length L radius R
-      - cap: length L radius R
-
-    Additionally, a body definition may be followed by extra quantities
-    specifying the position, orientation, and other quantities of the body.
-
-    - position X Y Z -- specifies the absolute location of the body in world
-      coordinates. Typically this is only given for one object. By default
-      bodies are all created at the origin.
-
-    - quaternion W X Y Z -- specifies the angle (in degrees) and axis of
-      rotation for the body. By default bodies are created without any rotation
-      (i.e., with a 0 1 0 0 quaternion).
-
-    - root -- indicates that this body is a root in the skeleton.
-
-    Joints
-    ------
-
-    A "join" section must contain the following information:
-
-    - A shape specification. This must be one of fixed, slider, hinge, piston,
-      universal, or ball. The corresponding ODE joint type will be used to join
-      together two bodies.
-
-    - Two attachment specifications. These specify the bodies to be joined, as
-      well as the body-relative offsets (on each body) where the joint will be
-      anchored.
-
-    Additionally, joints may specify information about their axes, rotation
-    limits, etc.
-
-    - axisN X [Y [Z]] -- specifies that axis number N (either 0, 1, or 2, and
-      depending on the type of joint) points along the vector X Y Z. By default,
-      axis0 points along 1 0 0, axis1 along 0 1 0, and axis2 along 0 0 1.
-
-    - lo_stops A [B [C]] -- specifies the minimum permitted rotation (in
-      degrees) for this joint along its respective axes.
-
-    - hi_stops A [B [C]] -- specifies the maximum permitted rotation (in
-      degrees) for this joint along its respective axes.
-
-    - stop_cfm N -- set the lo- and hi-stop CFM for DOFs on this joint.
-
-    - stop_erp N -- set the lo- and hi-stop ERP for DOFs on this joint.
-
-    - passive -- indicates that this joint's motor remains disabled.
-
-    Example
-    -------
-
-    A simple example is probably sufficient to get started; also see the
-    ``cooper-skeleton.txt`` file distributed with the pagoda source for a more
-    complete example.
-
-    | body box foo-body lengths 0.5 0.3 0.2    position 0 0 2
-    | body sph bar      radius 0.1
-    | body cap baz      radius 0.1 length 0.3  root
-
-    This section of the example causes three bodies to be created:
-
-    - A box identified as "foo-body" that is 50cm x 30cm x 20cm along the X, Y,
-      and Z axes, respectively. foo-body will be positioned at world coordinates
-      (0, 0, 2).
-
-    - A sphere identified as "bar" that is 10cm in radius.
-
-    - A capped cylinder identified as "baz" that is 10cm in radius and 30cm long
-      (the total length of this geometry is actually 50cm due to the
-      hemispherical cylinder end caps). This body is a root of the skeleton.
-
-    | join hinge  foo-body 1 0 0  bar 0 -1 0
-    | lo_stops -10
-    | hi_stops  20
-    | join ball   bar      0 0 0  baz 0  0 1
-
-    This section joins the three bodies together:
-
-    - The center of the positive-X side of foo-body is joined to the point on
-      the face of the bar sphere with the smallest Y coordinate, using a hinge
-      joint (1 DOF). This joint is constrained to rotate between -10 and +20
-      degrees.
-
-    - The center of bar is joined to the point on baz with the largest Z
-      coordinate using an unconstrained ball joint.
-
+    '''Base class for skeleton parsers of various sorts.
     '''
 
     def __init__(self, world, jointgroup=None):
@@ -250,6 +135,136 @@ class Parser:
         '''
         return [self._next_float() for _ in range(n)]
 
+    def parse(self, source):
+        '''Load and parse a source file.
+
+        Parameters
+        ----------
+        source : str or file
+            A filename or file-like object that contains text information
+            describing a skeleton.
+        '''
+        raise NotImplementedError
+
+
+class SkelParser(Parser):
+    '''This parser class reads a text configuration of a pagoda simulation.
+
+    The format for the configuration file follows basic Unix conventions. A
+    preprocessor discards any text on a line following the pound (#) character.
+    Blank lines are skipped. The remainder of the file is chunked into sections
+    based on keywords; each keyword in the file indicates the start of a new
+    section. The recognized keywords are:
+
+    - body -- indicates the definition of a new rigid body;
+    - join -- indicates that two bodies will be joined together.
+
+    Bodies
+    ------
+
+    A "body" section must contain the following information:
+
+    - A shape specification. This must be one of box (rectangular prism), sph or
+      sphere (sphere), cyl or cylinder (cylinder), or cap (capped cylinder).
+
+      The shape indicates the canonical geometry that will be used to (a)
+      specify the size of the body, and (b) detect collisions.
+
+    - An identifier for the body. This cannot contain spaces.
+
+    - One or more dimension parameters. Each of these consists of a shape
+      keyword followed by one or more size in meters. Different dimension
+      keywords are required based on the shape of the body:
+
+      - box: length X Y Z
+      - sph: radius R
+      - cyl: length L radius R
+      - cap: length L radius R
+
+    Additionally, a body definition may be followed by extra quantities
+    specifying the position, orientation, and other quantities of the body.
+
+    - position X Y Z -- specifies the absolute location of the body in world
+      coordinates. Typically this is only given for one object. By default
+      bodies are all created at the origin.
+
+    - quaternion W X Y Z -- specifies the angle (in degrees) and axis of
+      rotation for the body. By default bodies are created without any rotation
+      (i.e., with a 0 1 0 0 quaternion).
+
+    - root -- indicates that this body is a root in the skeleton.
+
+    Joints
+    ------
+
+    A "join" section must contain the following information:
+
+    - A shape specification. This must be one of fixed, slider, hinge, piston,
+      universal, or ball. The corresponding ODE joint type will be used to join
+      together two bodies.
+
+    - Two attachment specifications. These specify the bodies to be joined, as
+      well as the body-relative offsets (on each body) where the joint will be
+      anchored.
+
+    Additionally, joints may specify information about their axes, rotation
+    limits, etc.
+
+    - axisN X [Y [Z]] -- specifies that axis number N (either 0, 1, or 2, and
+      depending on the type of joint) points along the vector X Y Z. By default,
+      axis0 points along 1 0 0, axis1 along 0 1 0, and axis2 along 0 0 1.
+
+    - lo_stops A [B [C]] -- specifies the minimum permitted rotation (in
+      degrees) for this joint along its respective axes.
+
+    - hi_stops A [B [C]] -- specifies the maximum permitted rotation (in
+      degrees) for this joint along its respective axes.
+
+    - stop_cfm N -- set the lo- and hi-stop CFM for DOFs on this joint.
+
+    - stop_erp N -- set the lo- and hi-stop ERP for DOFs on this joint.
+
+    - passive -- indicates that this joint's motor remains disabled.
+
+    Example
+    -------
+
+    A simple example is probably sufficient to get started; also see the
+    ``cooper-skeleton.txt`` file distributed with the pagoda source for a more
+    complete example.
+
+    | body box foo-body lengths 0.5 0.3 0.2    position 0 0 2
+    | body sph bar      radius 0.1
+    | body cap baz      radius 0.1 length 0.3  root
+
+    This section of the example causes three bodies to be created:
+
+    - A box identified as "foo-body" that is 50cm x 30cm x 20cm along the X, Y,
+      and Z axes, respectively. foo-body will be positioned at world coordinates
+      (0, 0, 2).
+
+    - A sphere identified as "bar" that is 10cm in radius.
+
+    - A capped cylinder identified as "baz" that is 10cm in radius and 30cm long
+      (the total length of this geometry is actually 50cm due to the
+      hemispherical cylinder end caps). This body is a root of the skeleton.
+
+    | join hinge  foo-body 1 0 0  bar 0 -1 0
+    | lo_stops -10
+    | hi_stops  20
+    | join ball   bar      0 0 0  baz 0  0 1
+
+    This section joins the three bodies together:
+
+    - The center of the positive-X side of foo-body is joined to the point on
+      the face of the bar sphere with the smallest Y coordinate, using a hinge
+      joint (1 DOF). This joint is constrained to rotate between -10 and +20
+      degrees.
+
+    - The center of bar is joined to the point on baz with the largest Z
+      coordinate using an unconstrained ball joint.
+    '''
+
     def _handle_body(self):
         '''Parse the entirety of a "body" section in the source.'''
         shape = self._next_token(expect='^({})$'.format('|'.join(physics.BODIES)))
@@ -360,3 +375,9 @@ class Parser:
                 token = self._handle_joint()
             else:
                 self._error('unexpected token')
+
+
+class AsfParser(Parser):
+    '''Parse a skeleton definition in ASF format.
+    '''
+    pass
