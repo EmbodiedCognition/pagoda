e51b894896d1853306536cabd022b1a3df789ac3
Leif Johnson <leif@lmjohns3.com>
2015-02-27 16:45:19 -0600

    Use really crazy default velocity value.

--- pagoda/cooper.py
+++ pagoda/cooper.py
@@ -156,7 +156,7 @@ class Markers:
         '''Process data to produce velocity and dropout information.'''
         self.visibility = self.data[:, :, 3]
         self.positions = self.data[:, :, :3]
-        self.velocities = np.zeros_like(self.positions) + 10
+        self.velocities = np.zeros_like(self.positions) + 1000
         for frame_no in range(1, len(self.data) - 1):
             prev = self.data[frame_no - 1]
             next = self.data[frame_no + 1]
