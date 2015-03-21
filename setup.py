6dcbca289389e26682db6520664f60e3d951847c
Leif Johnson <leif@lmjohns3.com>
2015-03-20 21:41:00 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='pagoda',
-    version='0.0.2',
+    version='0.1.0',
     packages=setuptools.find_packages(),
     author='Leif Johnson',
     author_email='leif@cs.utexas.edu',
