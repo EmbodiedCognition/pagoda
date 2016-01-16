51a332d0c2cc7ca3bfce0eae05911b74a621eb33
Leif Johnson <leif@lmjohns3.com>
2016-01-15 20:15:12 -0600

    Set PKG_CONFIG_PATH to build ode extension.

--- build-ode-for-travis.sh
+++ build-ode-for-travis.sh
@@ -11,6 +11,6 @@ make -j
 make install
 
 cd bindings/python
-LD_LIBRARY_PATH=$HOME/ode/lib:$LD_LIBRARY_PATH \
+PKG_CONFIG_PATH=$HOME/ode/lib:$PKG_CONFIG_PATH \
     python setup.py build_ext -L$HOME/ode/lib -I$HOME/ode/include
 python setup.py install
