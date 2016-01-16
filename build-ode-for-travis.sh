b1a4ae8b9cd6ef567d9e13603782dca04aafe0a6
Leif Johnson <leif@lmjohns3.com>
2016-01-15 20:29:55 -0600

    Make build quieter. Try again with pkg_config_path

--- build-ode-for-travis.sh
+++ build-ode-for-travis.sh
@@ -5,12 +5,13 @@ svn checkout -r 1939 https://svn.code.sf.net/p/opende/code/trunk opende
 
 cd opende
 patch -p0 < ../ode-r1939.patch
-./bootstrap
-./configure --enable-double-precision --enable-shared --prefix=$HOME/ode
-make -j
+./bootstrap >/dev/null 2>&1
+./configure --enable-double-precision --enable-shared --prefix=$HOME/ode >/dev/null 2>&1
+make -j >/dev/null 2>&1
 make install
 
+export PKG_CONFIG_PATH=$HOME/ode/lib/pkgconfig:$HOME/ode/lib:$PKG_CONFIG_PATH
+
 cd bindings/python
-PKG_CONFIG_PATH=$HOME/ode/lib/pkgconfig:$PKG_CONFIG_PATH \
-    python setup.py build_ext -L$HOME/ode/lib -I$HOME/ode/include
+python setup.py build_ext -L$HOME/ode/lib -I$HOME/ode/include
 python setup.py install
