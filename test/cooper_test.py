e890ef358a1a730c67349f57b8ea5a0310010489
Leif Johnson <leif@lmjohns3.com>
2016-01-26 16:00:10 -0600

    fixup! Rename several Markers attributes:

--- test/cooper_test.py
+++ test/cooper_test.py
@@ -14,16 +14,16 @@ class TestMarkers(Base):
     def test_c3d(self):
         self.markers.load_c3d('examples/cooper-motion.c3d')
         assert self.markers.num_frames == 343
-        assert len(self.markers.marker_bodies) == 41
-        assert len(self.markers.attach_bodies) == 0
-        assert len(self.markers.attach_offsets) == 0
+        assert len(self.markers.bodies) == 41
+        assert len(self.markers.targets) == 0
+        assert len(self.markers.offsets) == 0
         assert len(self.markers.channels) == 41
 
     def test_csv(self):
         return  # TODO
         self.markers.load_csv('examples/cooper-motion.csv')
         assert self.markers.num_frames == 343
-        assert len(self.markers.marker_bodies) == 41
-        assert len(self.markers.attach_bodies) == 0
-        assert len(self.markers.attach_offsets) == 0
+        assert len(self.markers.bodies) == 41
+        assert len(self.markers.targets) == 0
+        assert len(self.markers.offsets) == 0
         assert len(self.markers.channels) == 41
