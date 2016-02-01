d92ee250f8ac1bdfde40c2fed00cf64e2bc5dc99
Leif Johnson <leif@lmjohns3.com>
2016-02-01 00:08:51 -0600

    Add ASF parser.

--- pagoda/parser.py
+++ pagoda/parser.py
@@ -38,7 +38,24 @@ def parse(source, world, jointgroup=None, density=1000, color=None):
     return visitor
 
 
-class Visitor(parsimonious.nodes.NodeVisitor):
+class NodeVisitor(parsimonious.nodes.NodeVisitor):
+    def generic_visit(self, node, children):
+        return node.text
+
+    def visit_(self, node, children):
+        return children
+
+    def visit_number(self, node, children):
+        return float(node.text)
+
+    def visit_float(self, node, children):
+        return float(node.text)
+
+    def visit_int(self, node, children):
+        return int(node.text)
+
+
+class Visitor(NodeVisitor):
     '''
     '''
 
@@ -58,21 +75,6 @@ class Visitor(parsimonious.nodes.NodeVisitor):
         self.joints = []
 
         self.attrs = {}
-
-    def generic_visit(self, node, children):
-        return node.text
-
-    def visit_(self, node, children):
-        return children
-
-    def visit_number(self, node, children):
-        return float(node.text)
-
-    def visit_float(self, node, children):
-        return float(node.text)
-
-    def visit_int(self, node, children):
-        return int(node.text)
 
     def visit_tuplecolor(self, node, children):
         r, _, g, _, b, rest = tuple(children)
@@ -260,7 +262,64 @@ class Visitor(parsimonious.nodes.NodeVisitor):
                      mass, vol, mass / vol)
 
 
-class AsfParser(object):
+def parse_asf(source, world, jointgroup=None, density=1000, color=None):
+    '''Load and parse a source file.
+
+    Parameters
+    ----------
+    source : file
+        A file-like object that contains text information describing bodies and
+        joints to add to the world.
+    world : :class:`pagoda.physics.World`
+        The world to add objects and joints to.
+    jointgroup : ode.JointGroup, optional
+        If provided, add all joints from this parse to the given group. The
+        default behavior adds joints to the world without an explicit group.
+    density : float, optional
+        Default density for bodies. This is overridden if the source provides a
+        density or mass value for a given body.
+    color : tuple of floats, optional
+        Default color for bodies from this source. Defaults to None, which does
+        not assign a color to parsed bodies.
+    '''
+    visitor = AsfVisitor(world, jointgroup, density, color)
+    visitor.parse(source.read())
+    return visitor
+
+
+class AsfBone(object):
+    '''A bone is an individual link in an ASF-specified skeleton.'''
+
+    def __init__(self):
+        self.id = None
+        self.name = ''
+        self.direction = []
+        self.length = -1
+        self.axis = []
+        self.order = 'XYZ'
+        self.dof = []
+        self.limits = []
+
+    @property
+    def end(self):
+        return self.direction * self.length
+
+    @property
+    def rotation(self):
+        m = np.eye(3)
+        for ax in self.order:
+            theta = self.axis['XYZ'.index(ax)]
+            ct, st = np.cos(theta), np.sin(theta)
+            if ax == 'X':
+                m = np.dot([[1, 0, 0], [0, ct, -st], [0, st, ct]], m)
+            if ax == 'Y':
+                m = np.dot([[ct, 0, st], [0, 1, 0], [-st, 0, ct]], m)
+            if ax == 'Z':
+                m = np.dot([[ct, -st, 0], [st, ct, 0], [0, 0, 1]], m)
+        return m
+
+
+class AsfVisitor(NodeVisitor):
     '''Parse a skeleton definition in ASF format.
 
     For a description of the file format, see
@@ -272,74 +331,194 @@ class AsfParser(object):
     a skeleton in the physics simulator.
     '''
 
-    TOPLEVEL_TOKENS = 'version name units documentation root bonedata hierarchy'.split()
-    TOPLEVEL_TOKEN_RE = r'^:({})$'.format('|'.join(TOPLEVEL_TOKENS))
+    grammar = parsimonious.grammar.Grammar(
+        open(os.path.join(os.path.dirname(__file__), 'asf.peg')).read())
 
-    def parse(self, source):
-        '''Load and parse a source file.
+    def __init__(self, world, jointgroup=None, density=1000, color=None,
+                 *args, **kwargs):
+        super(AsfVisitor, self).__init__(*args, **kwargs)
 
-        Parameters
-        ----------
-        source : str or file
-            A filename or file-like object that contains text information
-            describing a skeleton.
-        '''
-        toplevel = AsfParser.TOPLEVEL_TOKEN_RE
-        self.load(source)
-        token = self._next_token(expect=toplevel)
-        while token is not None:
-            if token == ':version':
-                self.version = self._next_token()
-                token = self._next_token(expect=toplevel)
-            elif token == ':name':
-                self.name = self._next_token()
-                token = self._next_token(expect=toplevel)
-            elif token == ':units':
-                token = self._handle_units()
-            elif token == ':documentation':
-                token = self._handle_documentation()
-            elif token == ':root':
-                token = self._handle_root()
-            elif token == ':bonedata':
-                token = self._handle_bonedata()
-            elif token == ':hierarchy':
-                token = self._handle_hierarchy()
-            else:
-                self._error('unexpected token')
+        self.world = world
+        self.jointgroup = jointgroup
+        self.density = density
+        self.color = color
 
-    def _handle_units(self):
-        units = {}
-        token = self._next_token()
-        while token:
-            value = self._next_token()
-            if re.match(FLOAT_RE, value):
-                value = float(value)
-            units[token] = value
-            token = self._next_token()
-            if re.match(AsfParser.TOPLEVEL_TOKEN_RE, token):
-                break
-        self.units = units
-        return token
+        self.version = ''
+        self.name = ''
+        self.units = {}
+        self.root = {}
+        self.documentation = ''
 
-    def _handle_documentation(self):
-        token = self._next_token()
-        tokens = []
-        while token:
-            if re.match(AsfParser.TOPLEVEL_TOKEN_RE, token):
-                break
-            tokens.append(token)
-            token = self._next_token()
-        self.documentation = ' '.join(tokens)
-        return token
+        self.bodies = []
+        self.joints = []
 
-    def _handle_root(self):
-        raise NotImplementedError
+        self.bone = AsfBone()
+        self.bones = {}
+        self.hierarchy = {}
 
-    def _handle_bonedata(self):
-        raise NotImplementedError
+    def visit_version(self, node, children):
+        self.version = children[-1]
 
-    def _handle_bone(self):
-        raise NotImplementedError
+    def visit_asf_name(self, node, children):
+        self.name = children[-1]
 
-    def _handle_hierarchy(self):
-        raise NotImplementedError
+    def visit_documentation(self, node, children):
+        self.documentation = ''.join(s + w for s, w in children[-1]).strip()
+
+    def visit_mass_unit(self, node, children):
+        self.units['mass'] = children[-1]
+
+    def visit_length_unit(self, node, children):
+        self.units['length'] = children[-1]
+
+    def visit_angle_unit(self, node, children):
+        self.units['angle'] = children[-1]
+
+    def visit_root_axis(self, node, children):
+        self.root['axis'] = children[-1][0]
+
+    def visit_order(self, node, children):
+        self.root['order'] = [dof for _, dof in children[-1]]
+
+    def visit_position(self, node, children):
+        _, _, x, _, y, _, z = children
+        self.root['position'] = x, y, z
+
+    def visit_orientation(self, node, children):
+        _, _, x, _, y, _, z = children
+        self.root['orientation'] = x, y, z
+
+    def visit_bone(self, node, children):
+        self.bones[self.bone.name] = self.bone
+        self.bone = AsfBone()
+
+    def visit_id(self, node, children):
+        self.bone.id = children[-1]
+
+    def visit_name(self, node, children):
+        self.bone.name = children[-1]
+
+    def visit_length(self, node, children):
+        self.bone.length = children[-1]
+
+    def visit_direction(self, node, children):
+        _, _, x, _, y, _, z = children
+        self.bone.direction = x, y, z
+
+    def visit_axis(self, node, children):
+        _, _, rx, _, ry, _, rz, _, order = children
+        self.bone.axis = rx, ry, rz
+        self.bone.order = order
+
+    def visit_dofs(self, node, children):
+        self.bone.dof = [dof for _, dof in children[-1]]
+
+    def visit_limits(self, node, children):
+        self.bone.limits = children[-1]
+
+    def visit_limit(self, node, children):
+        _, _, lo, _, hi, _, _ = children
+        return lo, hi
+
+    def visit_chain(self, node, children):
+        parent, children, _ = children
+        self.hierarchy[parent] = [id for _, id in children]
+
+    @property
+    def scale(self):
+        '''Return a factor to convert length-scaled inches to mm.'''
+        return 2.54 / (100. * self.units['length'])
+
+    def create_bodies(self, translate=(0, 1, 0), size=0.1):
+        '''Traverse the bone hierarchy and create physics bodies.'''
+        stack = [('root', 0, self.root['position'] + translate)]
+        while stack:
+            name, depth, end = stack.pop()
+
+            for child in self.hierarchy.get(name, ()):
+                stack.append((child, depth + 1, end + self.bones[child].end))
+
+            if name not in self.bones:
+                continue
+
+            bone = self.bones[name]
+            body = self.world.create_body(
+                'box', name=bone.name, density=density,
+                lengths=(size, size, bone.length))
+
+            # move the center of the body to the halfway point between
+            # the parent (joint) and child (joint).
+            x, y, z = end - bone.direction * bone.length / 2
+
+            # swizzle y and z -- asf uses y as up, but we use z as up.
+            body.position = x, z, y
+
+            # compute an orthonormal (rotation) matrix using the ground and
+            # the body. this is mind-bending but seems to work.
+            u = bone.direction
+            v = np.cross(u, [0, 1, 0])
+            l = np.linalg.norm(v)
+            if l > 0:
+                v /= l
+                rot = np.vstack([np.cross(u, v), v, u]).T
+                swizzle = [[1, 0, 0], [0, 0, 1], [0, -1, 0]]
+                body.rotation = np.dot(swizzle, rot).flatten()
+
+            self.bodies.append(body)
+
+    def create_joints(self):
+        '''Traverse the bone hierarchy and create physics joints.'''
+        stack = ['root']
+        while stack:
+            parent = stack.pop()
+            for child in self.hierarchy.get(parent, ()):
+                stack.append(child)
+            if parent not in self.bones:
+                continue
+            bone = self.bones[parent]
+            body = [b for b in self.bodies if b.name == parent][0]
+            for child in self.hierarchy.get(parent, ()):
+                child_bone = self.bones[child]
+                child_body = [b for b in self.bodies if b.name == child][0]
+                shape = ('', 'hinge', 'universal', 'ball')[len(child_bone.dof)]
+                self.joints.append(self.world.join(shape, body, child_body))
+
+
+def parse_amc(source):
+    '''Parse an AMC motion capture data file.
+
+    Parameters
+    ----------
+    source : file
+        A file-like object that contains AMC motion capture text.
+
+    Yields
+    ------
+    frame : dict
+        Yields a series of motion capture frames. Each frame is a dictionary
+        that maps a bone name to a list of the DOF configurations for that bone.
+    '''
+    lines = 0
+    frames = 1
+    frame = {}
+    degrees = False
+    for line in source:
+        lines += 1
+        line = line.split('#')[0].strip()
+        if not line:
+            continue
+        if line.startswith(':'):
+            if line.lower().startswith(':deg'):
+                degrees = True
+            continue
+        if line.isdigit():
+            if int(line) != frames:
+                raise RuntimeError(
+                    'frame mismatch on line {}: '
+                    'produced {} but file claims {}'.format(lines, frames, line))
+            yield frame
+            frames += 1
+            frame = {}
+            continue
+        fields = line.split()
+        frame[fields[0]] = list(map(float, fields[1:]))
